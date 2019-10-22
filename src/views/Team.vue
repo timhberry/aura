0d07ad980560cfdcd27c4fce67f337cd4ab576b5
Tim Berry <timhberry@gmail.com>
2019-10-22 15:41:09 +0100

    Fix team page

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -4,11 +4,8 @@
     <v-container fluid class="mt-3 mb-0">
       <v-layout wrap align-center justify-center row fill-height class="my-0">
         <v-flex xs12 md10 class="my-0">
-            <p class="google-font mb-0" style="font-size:200%;color:#616161">Our lineup of big thinkers and even bigger doers</p>
-            <p class="google-font mt-0 mb-0" style="font-size:110%">Google is known all around the world. Everyone is 'googling', checking on 'maps' and communicating in 'gmail'. For simple users, they are services that just works, but not for us. Developers see much more: APIs, scalability issues, complex technology stacks. And that is what GDG is about. </p>
-
-            <p class="google-font mt-3 mb-0" style="font-size:110%">Our goal is to organize space to connect the best industry experts with Indian audience to boost development of IT. And Our Core Team is: </p>
-
+            <p class="google-font mb-0" style="font-size:200%;color:#616161">Our noble organisers</p>
+            <p class="google-font mt-0 mb-0" style="font-size:110%">These brave souls selflessly give up entire hours of their free time to organise GDG meetups, persitently nagging venue sponsors, ordering pizza, and bribing speakers with Swag. If you see them, just nod sagely, and know that they carry the weight of this medium-sized community group on their not inconsiderable shoulders.</p>
         </v-flex>
       </v-layout>
     </v-container>
